2b1f60858a58c6f21a056774e8d5034223eceb53
Atish <atish6808@gmail.com>
2023-01-05 14:21:48 +0530

    frontend work 1/2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
 import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"
 
+
+const Header = styled.div`
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  padding: 20px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+`;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const AppName = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const SearchIcon = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+const RecipeImage = styled.img`
+  width: 36px;
+  height: 36px;
+  margin: 15px;
+`;
+const Search = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const SearchInput = styled.input`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+  margin-left: 15px;
+`;
+const SearchBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 10px;
+  border-radius: 6px;
+  margin-left: 20px;
+  width: 50%;
+  background-color: white;
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+        <AppName>
+        <RecipeImage src="./assets/hamburger.svg"></RecipeImage>
+          Food Assistant
+          </AppName>
+        <SearchBox>
+          <SearchIcon src="./assets/search-icon.svg"></SearchIcon>
+        <SearchInput placeholder='Type an ingredient!'></SearchInput>
+        </SearchBox>
+        </Header>
+    </Container>
+    
   );
 }
 
