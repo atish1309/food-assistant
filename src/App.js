552881451918c166f523019ff8cb875787e293cd
Atish <atish6808@gmail.com>
2023-01-06 08:45:44 +0530

    added some css changes, images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import styled from "styled-components"
-
-
+import hamburgassets from './assets/hamburger.svg'
+import searchicon from './assets/search-icon.svg'
 const Header = styled.div`
   background-color: black;
   color: white;
@@ -13,6 +13,33 @@ const Header = styled.div`
   font-size: 25px;
   font-weight: bold;
   box-shadow: 0 3px 6px 0 #555;
+`;
+const Titletext = styled.span`
+  color: black;
+  font-weight: bold;
+  font-size: 25px;
+  margin-bottom: 12px;
+  padding: 5px 15px;
+`;
+
+const IngredientsText = styled.span`
+  color: green;
+  border: solid 1px green;
+  cursor:pointer;
+  font-weight: bold;
+  text-align:center;
+  margin-bottom: 12px;
+  padding: 7px 15px;
+  border-radius:5px;
+`;
+const SeeNewTab = styled.span`
+  color: red;
+  cursor:pointer;
+  text-align:center;
+  font-weight: bold;
+  border: solid 1px red;
+  padding: 7px 15px;
+  border-radius:5px;
 `;
 const Container = styled.div`
   display: flex;
@@ -31,6 +58,22 @@ const RecipeImage = styled.img`
   width: 36px;
   height: 36px;
   margin: 15px;
+`;
+const RecipeListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 30px;
+  gap: 20px;
+  flex-wrap:wrap;
+  justify-content: space-evenly;
+`;
+const RecipeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  align-items: center;
+  width: 300px;
+  box-shadow: 0 3px 10px 0 #aaa;
 `;
 const Search = styled.div`
   display: flex;
@@ -54,19 +97,32 @@ const SearchBox = styled.div`
   width: 50%;
   background-color: white;
 `;
+
+const CoverImage = styled.img`
+  object-fit: cover;
+  height: 200px;
+`;
 function App() {
   return (
     <Container>
       <Header>
         <AppName>
-        <RecipeImage src="./assets/hamburger.svg"></RecipeImage>
+        <RecipeImage src={hamburgassets}></RecipeImage>
           Food Assistant
           </AppName>
         <SearchBox>
-          <SearchIcon src="./assets/search-icon.svg"></SearchIcon>
+          <SearchIcon src={searchicon}></SearchIcon>
         <SearchInput placeholder='Type an ingredient!'></SearchInput>
         </SearchBox>
         </Header>
+        <RecipeListContainer>
+          <RecipeContainer>
+        <CoverImage src={hamburgassets}/>
+        <Titletext >Matar Paneer</Titletext>
+        <IngredientsText>Ingredients</IngredientsText>
+        <SeeNewTab>Recipe</SeeNewTab>
+        </RecipeContainer>
+        </RecipeListContainer>
     </Container>
     
   );
